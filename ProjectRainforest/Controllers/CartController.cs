ffcd14b0933a350bcf91b118693226eb57f12ac0
Taha Iftikhar <Iftikhta@sheridancollege.ca>
2020-12-04 12:52:03 -0500

    added cart controller

---
+++ ProjectRainforest/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectRainforest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectRainforest.Controllers
+{
+    public class CartController : Controller
+    {
+        public static RainforestDBContext context = new RainforestDBContext();
+
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
