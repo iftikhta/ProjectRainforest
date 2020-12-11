deb876b2612f8e96b6e0ec46e0d45ab6cfd92d3e
JScrivo <scrivjor@sheridancollege.ca>
2020-12-11 14:50:13 -0500

    Change Address page made

--- ProjectRainforest/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ ProjectRainforest/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -12,6 +12,8 @@ namespace ProjectRainforest.Areas.Identity.Pages.Account.Manage
 
         public static string Email => "Email";
 
+        public static string Address => "Address";
+
         public static string ChangePassword => "ChangePassword";
 
         public static string DownloadPersonalData => "DownloadPersonalData";
@@ -27,6 +29,8 @@ namespace ProjectRainforest.Areas.Identity.Pages.Account.Manage
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
+
+        public static string AddressNavClass(ViewContext viewContext) => PageNavClass(viewContext, Address);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
