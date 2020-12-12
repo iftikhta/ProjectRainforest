76615deab8ea832831011373db0b2e9a63e2edda
JScrivo <scrivjor@sheridancollege.ca>
2020-12-12 18:58:02 -0500

    Enforced Authorization

--- ProjectRainforest/Controllers/OrderController.cs
+++ ProjectRainforest/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -13,6 +14,7 @@ using TaxServiceReference;
 
 namespace ProjectRainforest.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         public static RainforestDBContext context = new RainforestDBContext();
