03310338087d5c5479fb43d9b7e645076d08f274
Taha Iftikhar <Iftikhta@sheridancollege.ca>
2020-12-31 14:43:31 -0500

    1.Tutor model created 2. Working on AddProductsFromAPI

---
+++ ProjectRainforest/Models/Tutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectRainforest.Models
+{
+    public class Tutor
+    {
+        public int Tutor_id { get; set; }
+        public string Tutor_name { get; set; }
+        public string Tutor_description { get; set; }
+        public string Tutor_Subjects { get; set; }
+        public int Tutor_rate { get; set; }
+        public string Tutor_img { get; set; }
+        public int Tutor_rating  { get; set; }
+        public DateTime Tutor_date_joined { get; set; }
+
+    }
+}
